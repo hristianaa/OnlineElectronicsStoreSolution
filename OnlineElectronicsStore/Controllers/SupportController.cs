b3c00ab7df9ebce2b96ef3c7d697f004428074b8
hristianaa <hristiana.hristova@icloud.com>
2025-05-13 20:37:43 +0300

    feat(support): add SupportController with Help and Contact actions

--- OnlineElectronicsStore/Controllers/SupportController.cs
+++ OnlineElectronicsStore/Controllers/SupportController.cs
@@ -1,6 +1,44 @@
-﻿namespace OnlineElectronicsStore.Controllers
+﻿// Controllers/SupportController.cs
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineElectronicsStore.Data;
+using OnlineElectronicsStore.Models;
+using OnlineElectronicsStore.Models.ViewModels;
+
+namespace OnlineElectronicsStore.Controllers
 {
-    public class SupportController
+    public class SupportController : Controller
     {
+        private readonly AppDbContext _db;
+        public SupportController(AppDbContext db) => _db = db;
+
+        // GET: /Support/Contact
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            ViewBag.Success = TempData["SupportSuccess"];
+            return View(new ContactFormModel());
+        }
+
+        // POST: /Support/Contact
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Contact(ContactFormModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            // map & save
+            var msg = new SupportMessage
+            {
+                Name = vm.Name,
+                Email = vm.Email,
+                Message = vm.Message
+            };
+            _db.SupportMessages.Add(msg);
+            await _db.SaveChangesAsync();
+
+            TempData["SupportSuccess"] = "Your message has been sent, thank you!";
+            return RedirectToAction(nameof(Contact));
+        }
     }
 }
