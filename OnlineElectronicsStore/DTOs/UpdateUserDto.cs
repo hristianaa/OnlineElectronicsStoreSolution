36552a9159555753b94a5b57d41c2b117766082f
hristianaa <hristiana.hristova@icloud.com>
2025-05-13 13:02:23 +0300

    Add UpdateUserDto for encapsulating profile update data

---
+++ OnlineElectronicsStore/DTOs/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineElectronicsStore.DTOs
+{
+    public class UpdateUserDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? NewPassword { get; set; }
+    }
+}
