01dfafa18e418c0854a4aea27ab02cb8e3572295
hristianaa <hristiana.hristova@icloud.com>
2025-05-13 20:37:43 +0300

    feat(support): add ContactFormModel for user contact form

--- OnlineElectronicsStore/Models/ViewModels/ContactFormModel.cs
+++ OnlineElectronicsStore/Models/ViewModels/ContactFormModel.cs
@@ -1,6 +1,17 @@
-﻿namespace OnlineElectronicsStore.Models.ViewModels
+﻿// Models/ViewModels/ContactFormModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineElectronicsStore.Models.ViewModels
 {
     public class ContactFormModel
     {
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, StringLength(2000)]
+        public string Message { get; set; }
     }
 }
