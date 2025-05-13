14f3e1c2511408ab87a2b011d72f81f5c77c7ce7
hristianaa <hristiana.hristova@icloud.com>
2025-05-13 13:02:24 +0300

    Add ProfileViewModel for binding and validating user profile edits

---
+++ OnlineElectronicsStore/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineElectronicsStore.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
