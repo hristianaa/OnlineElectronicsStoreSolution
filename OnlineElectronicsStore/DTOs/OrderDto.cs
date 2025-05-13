ab191fe425398f817ac11422fcde9ae8051795dd
hristianaa <hristiana.hristova@icloud.com>
2025-05-13 23:36:42 +0300

    added a order dto

---
+++ OnlineElectronicsStore/DTOs/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿// DTOs/OrderDto.cs
+namespace OnlineElectronicsStore.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; } = "";
+        public List<OrderLineDto> Items { get; set; } = new();
+        public decimal Total => Items.Sum(i => i.Quantity * i.UnitPrice);
+    }
+
+    public class OrderLineDto
+    {
+        public string ProductName { get; set; } = "";
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
